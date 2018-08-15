d377db8b7afde6d0c464f85998d600caf78b6def
钟乘永 <zhongcy93@gmail.com>
2018-08-15 08:53:56 +0800

    delete build dir

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zhongchengyong/opt/projects/Buy-my-ticket
-# Build directory: /Users/zhongchengyong/opt/projects/Buy-my-ticket/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
